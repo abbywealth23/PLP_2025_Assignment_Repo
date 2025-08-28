71209e315b32fe9c68f700bce6a7d23d7cdadaa0
Abbywealth23 <abbywealth23@gmail.com>
2025-08-28 18:50:22 +0100

    Week 5 Assignment update

---
+++ week 5 Assignment.py
@@ -0,0 +1,31 @@
+class House:
+        def __init__(self, type, block, streetName):
+                self.type = type
+                self.block = block
+                self.streetName = streetName
+
+class Apartment(House):
+                pass
+
+house1 = House("Bungalow", 3, "Ayonuga,fadeyi")
+house2 = Apartment("2-StoreyBuilding", 15, "Allen Avenue")
+
+print(house1.block)
+print(house2.streetName)
+
+
+
+#Activity 2
+
+class Car:
+    def move(self):
+      return "Driving!"
+
+class Plane:
+   def move(self):
+      return "Flying!"  
+          
+for animal in [Car(), Plane()]:
+  print(animal.move())
+  
+
